2a03d5a94ce75057abdbdf3215a4cdfc5e3ae26f
Joseph Rawson <joseph.rawson.works@littledebian.org>
2013-08-23 09:48:41 -0500

    fix context menu

--- mslemon/views/consultant/base.py
+++ mslemon/views/consultant/base.py
@@ -3,7 +3,7 @@ from mslemon.views.base import prepare_layout, make_ctx_menu
 def prepare_base_layout(request):
     layout = request.layout_manager.layout
     prepare_layout(layout)
-    layout.ctx_menu = make_ctx_menu(request).output()
+    layout.ctx_menu = make_ctx_menu(request)
     #url = request.route_url('view_wiki')
     #layout.ctx_menu.append_new_entry('Wiki', url)
     layout.title = 'Consultant'
