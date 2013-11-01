22736c18136a52354b815b58675d2066f8e32467
Joseph Rawson <joseph.rawson.works@littledebian.org>
2013-11-01 10:40:14 -0500

    added code to restore database

--- mslemon/managers/admin/dbadmin.py
+++ mslemon/managers/admin/dbadmin.py
@@ -1,12 +1,15 @@
 import os
 import cPickle as Pickle
+from zipfile import ZipFile, ZIP_DEFLATED
 import csv
 from ConfigParser import ConfigParser
 from StringIO import StringIO
+from datetime import datetime
 
 from sqlalchemy.orm.exc import NoResultFound
 import transaction
 
+from dateutil.parser import parse as dtparse
 
 from mslemon.models.base import DBSession
 
@@ -17,7 +20,6 @@ from mslemon.models.usergroup import UserGroup, UserConfig
 # sitecontent
 from mslemon.models.sitecontent import SiteImage
 from mslemon.models.sitecontent import SiteText
-
 
 # cases
 from mslemon.models.misslemon import Case, CaseTicket, CaseDocument
@@ -38,7 +40,7 @@ from mslemon.models.misslemon import Contact
 from mslemon.models.misslemon import ClientContact, GlobalContact
 from mslemon.models.misslemon import GroupContact, UserContact
 
-#FIXME: better module name
+#FIXME: better module namelist
 # documents
 from mslemon.models.misslemon import File, ScannedDocument
 from mslemon.models.misslemon import NamedDocument, ClientDocument
@@ -48,6 +50,7 @@ from mslemon.models.misslemon import UnassignedDocument
 from mslemon.models.misslemon import PhoneCall
 from mslemon.models.misslemon import ContactCall, ClientCall
 
+             
 # tickets
 from mslemon.models.misslemon import Description
 from mslemon.models.misslemon import Ticket, TicketCurrentStatus
@@ -56,6 +59,19 @@ from mslemon.models.misslemon import TicketDocument
 
 # misc
 from mslemon.models.misslemon import Address
+
+DBOBJECTS = ['User', 'Group', 'Password',
+             'UserGroup', 'UserConfig', 'SiteImage',
+             'SiteText', 'Case', 'CaseTicket', 'CaseDocument',
+             'CaseUser', 'CaseStatusChange', 'CaseCurrentStatus',
+             'CaseEvent', 'Client', 'Event', 'EventUser',
+             'Contact', 'ClientContact', 'GlobalContact',
+             'GroupContact', 'UserContact', 'File', 'ScannedDocument',
+             'UnassignedDocument', 'PhoneCall', 'ContactCall',
+             'ClientCall', 'Description', 'Ticket', 'TicketCurrentStatus',
+             'TicketStatusChange', 'TicketDocument', 'Address']
+
+             
 
 
 class DBAdminManager(object):
@@ -108,32 +124,22 @@ class DBAdminManager(object):
                     UserContact=UserContact)
         return self._serialize_objects(omap)
 
-    def backup_files(self, dirname):
-        if not os.path.isdir(dirname):
-            os.makedirs(dirname)
-        for fid in self.session.query(File.id):
-            filename = 'file-%d.pickle.gz'  % fid.id
-            fullname = os.path.join(dirname, filename)
-            f = self.session.query(File).get(fid.id)
-            print "Writing", fullname, len(f.content)
-            with gzip.GzipFile(fullname, 'w') as outfile:
-                #with bz2.BZ2File(fullname, 'w') as outfile:
-                #with file(fullname, 'w') as outfile:
-                data = f.serialize()
-                Pickle.dump(data, outfile)
-
-    def bfiles(self, dirname):
-        if not os.path.isdir(dirname):
-            os.makedirs(dirname)
-        for fid in self.session.query(File.id):
-            filename = 'file-%d'  % fid.id
-            fullname = os.path.join(dirname, filename)
-            f = self.session.query(File).get(fid.id)
-            print "Writing", fullname, len(f.content)
-            with file(fullname, 'w') as outfile:
-                outfile.write(f.content)
-
-                
+    def backup_file(self, zfile, file_id):
+        filename = 'files/file-%d' % file_id
+        f = self.session.query(File).get(file_id)
+        print "Writing", filename, len(f.content)
+        zfile.writestr(filename, f.content)
+        return dict(id=f.id, info=f.info)
+    
+    
+    def backup_files(self, zfile):
+        filelist = list()
+        for file_id in self.session.query(File.id):
+            data = self.backup_file(zfile, file_id.id)
+            filelist.append(data)
+        #zfile.writestr('fileinfo.pickle', Pickle.dumps(filelist))
+        return filelist
+    
     def backup_documents(self):
         omap = dict(ScannedDocument=ScannedDocument,
                     NamedDocument=NamedDocument,
@@ -159,16 +165,137 @@ class DBAdminManager(object):
         return self._serialize_objects(omap)
 
     
-
-    def backup(self):
+    # this is for small databases
+    def backup_objects(self):
         data = dict()
         for bf in [self.backup_usergroup,
                    self.backup_sitecontent, self.backup_cases,
                    self.backup_clients, self.backup_events,
                    self.backup_contacts, self.backup_documents,
+                   self.backup_phonecalls,
                    self.backup_tickets, self.backup_misc]:
             data.update(bf())
         return data
+
+    def backup(self, filename):
+        #zipfileobj = file(filename, 'w')
+        zipfile = ZipFile(filename, 'w', ZIP_DEFLATED)
+        data = self.backup_objects()
+        flist = self.backup_files(zipfile)
+        data['File'] = flist
+        zipfile.writestr('database', Pickle.dumps(data))
+        zipfile.close()
+
+    def _set_dbobject(self, dbobject, data):
+        for key, value in data.items():
+            if key in ['created', 'modified', 'attached',
+                       'last_change', 'received', 'changed']:
+                value = dtparse(value)
+            setattr(dbobject, key, value)
+            
+    def restore(self, filename):
+        User.metadata.drop_all()
+        User.metadata.create_all()
+        zipfile = ZipFile(filename, 'r')
+        dbdata = Pickle.loads(zipfile.read('database'))
+        with transaction.manager:
+            # usergroup objects
+            for data in dbdata['User']:
+                user = User(data['username'])
+                self._set_dbobject(user, data)
+                self.session.add(user)
+            for data  in dbdata['UserConfig']:
+                user_id = data['user_id']
+                text = data['text']
+                uc = UserConfig(user_id, text)
+                self.session.add(uc)
+            for data in dbdata['Password']:
+                pw = Password(data['user_id'], data['password'])
+                self.session.add(pw)
+            for data in dbdata['Group']:
+                g = Group(data['name'])
+                self._set_dbobject(g, data)
+                self.session.add(g)
+            for data in dbdata['UserGroup']:
+                ug = UserGroup(data['group_id'], data['user_id'])
+                self.session.add(ug)
+            # site content objects
+            for data in dbdata['SiteImage']:
+                si = SiteImage(data['name'], data['content'])
+                self._set_dbobject(si, data)
+                self.session.add(si)
+            for data in dbdata['SiteText']:
+                st = SiteText(data['name'], data['content'])
+                self._set_dbobject(st, data)
+                self.session.add(st)
+            #########################################
+            # mslemon objects
+            #########################################
+            for data in dbdata['Address']:
+                a = Address(data['street'])
+                self._set_dbobject(a, data)
+                self.session.add(a)
+            for data in dbdata['Contact']:
+                c = Contact(data['firstname'])
+                self._set_dbobject(c, data)
+                self.session.add(c)
+            for data in dbdata['GlobalContact']:
+                gc = GlobalContact(data['id'])
+                self.session.add(gc)
+            for data in dbdata['Client']:
+                c = Client(data['name'], data['contact_id'])
+                self._set_dbobject(c, data)
+                self.session.add(c)
+            for data in dbdata['Event']:
+                e = Event(data['title'])
+                self._set_dbobject(e, data)
+                self.session.add(e)
+            empty_init_objects = ['GroupContact',
+                                  'UserContact',
+                                  'ClientContact',
+                                  'EventUser',
+                                  'Description',
+                                  'Ticket',
+                                  'TicketStatusChange',
+                                  'TicketCurrentStatus',
+                                  'PhoneCall',
+                                  'ContactCall',
+                                  'ClientCall',
+                                  'File',
+                                  'ScannedDocument',
+                                  'NamedDocument',
+                                  'UnassignedDocument',
+                                  'ClientDocument',
+                                  'TicketDocument',
+                                  'Case',
+                                  'CaseStatusChange',
+                                  'CaseCurrentStatus',
+                                  'CaseUser',
+                                  'CaseTicket',
+                                  'CaseDocument',
+                                  'CaseEvent',
+                                  ]
+            for classname in empty_init_objects:
+                dbclass = eval(classname)
+                for data in dbdata[classname]:
+                    dbobj = dbclass()
+                    self._set_dbobject(dbobj, data)
+                    self.session.add(dbobj)
+                    
+            
+
+    
     
         
         
+DBOBJECTS = ['User', 'Group', 'Password',
+             'UserGroup', 'UserConfig', 'SiteImage',
+             'SiteText', 'Case', 'CaseTicket', 'CaseDocument',
+             'CaseUser', 'CaseStatusChange', 'CaseCurrentStatus',
+             'CaseEvent', 'Client', 'Event', 'EventUser',
+             'Contact', 'ClientContact', 'GlobalContact',
+             'GroupContact', 'UserContact', 'File', 'ScannedDocument',
+             'UnassignedDocument', 'PhoneCall', 'ContactCall',
+             'ClientCall', 'Description', 'Ticket', 'TicketCurrentStatus',
+             'TicketStatusChange', 'TicketDocument', 'Address']
+
